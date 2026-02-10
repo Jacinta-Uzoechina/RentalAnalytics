73cb5cbc6e217e9a25c6f3a184a567546874f1bf
Jacinta-Uzoechina <150396748+Jacinta-Uzoechina@users.noreply.github.com>
2026-02-10 22:21:33 +0100

    Update SqlQueries.sql

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -17,4 +17,28 @@ SELECT
 		AND fc.category_id = sc.category_id
 	LEFT JOIN rental r ON i.inventory_id = r.inventory_id
 	WHERE  r.rental_id IS NULL
-	GROUP BY sc.store_id, sc.category_name
+	GROUP BY sc.store_id, sc.category_name;
+
+-- Best categories
+WITH category_revenue AS (
+	SELECT 
+		SUM(p.amount) AS total_revenue_per_category,
+		c.name AS category
+	FROM payment p
+	LEFT JOIN rental r ON p.rental_id = r.rental_id
+	LEFT JOIN inventory i ON r.inventory_id = i.inventory_id
+	LEFT JOIN film_category fc ON i.film_id = fc.film_id
+	LEFT JOIN category c ON fc.category_id = c.category_id
+	GROUP BY c.name
+),
+average_category_revenue AS (
+	SELECT AVG(cr.total_revenue_per_category) AS average_category_revenue
+	FROM category_revenue cr
+)
+SELECT 
+	cr.category, 
+	cr.total_revenue_per_category AS revenue
+FROM category_revenue cr
+CROSS JOIN average_category_revenue acr
+WHERE cr.total_revenue_per_category > acr.average_category_revenue
+ORDER BY cr.total_revenue_per_category DESC;
